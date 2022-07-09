e9e1870f627a5759af68cd79731f06c531e98323
Bastian <bastian.wunderlich81002@gmail.com>
2022-07-09 19:42:59 +0200

    first commit

---
+++ HomeDriveAPI/Controllers/DriveController.cs
@@ -0,0 +1,50 @@
+﻿using HomeDriveAPI.FileManagment;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace HomeDriveAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DriveController : ControllerBase
+    {
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult SayHello()
+        {
+            return Ok("hello from controller");
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult GetAllFiles()
+        {
+            return Ok(Filemanager.ListFiles());
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public ActionResult UploadFiles(List<IFormFile> files)
+        {
+            Filemanager.SaveFilesAsync(files);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("[action]")]
+        public ActionResult DeleteFiles(List<string> pathsOfFilesToDelete)
+        {
+            Filemanager.DeleteFilesAsync(pathsOfFilesToDelete);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult DownloadFile([FromHeader] string pathOfFileToDownload)
+        {
+            var filedata = Filemanager.ReturnFile(pathOfFileToDownload);
+            var contentType = "multipart/form-data";
+            return File(filedata, contentType);
+        }
+    }
+}
---
+++ HomeDriveAPI/FileManagment/Filemanager.cs
@@ -0,0 +1,80 @@
+﻿
+namespace HomeDriveAPI.FileManagment
+{
+    public class Filemanager
+    {
+        private static readonly object _lockForSaving = new object();
+        private static readonly object _lockForDeleting = new object();
+        private static readonly string _drivepath = "drive";
+        public static bool SaveFilesAsync(List<IFormFile> files)
+        {
+            try
+            {
+                foreach (var file in files)
+                {
+                    lock (_lockForSaving) {
+                        var fStream = File.Create($"{_drivepath}/{file.FileName}");
+                        file.CopyTo(fStream);
+                        fStream.Flush();
+                    }
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool DeleteFilesAsync(List<string> files)
+        {
+            try
+            {
+                foreach (var file in files)
+                {
+                    lock (_lockForDeleting)
+                    {
+                        if (File.Exists($"{_drivepath}/{file}"))
+                            File.Delete($"{_drivepath}/{file}");
+                    }
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static byte[] ReturnFile(string file)
+        {
+            byte[] bytes = {};
+            try
+            {
+                lock (_lockForDeleting)lock(_lockForSaving)
+                {
+                    if (File.Exists($"{_drivepath}/{file}"))
+                    {
+                        byte[] buffer = null;
+                        using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+                        {
+                            buffer = new byte[fs.Length];
+                            fs.Read(buffer, 0, (int)fs.Length);
+                        }
+                    }
+                    return bytes;
+                }
+            }
+            catch (Exception)
+            {
+                return bytes;
+            }
+        }
+
+        public static List<string> ListFiles()
+        {
+            var dirInfo = new DirectoryInfo(_drivepath);
+            return dirInfo.GetFiles().Select(f => f.Name).ToList();
+        }
+    }
+}
